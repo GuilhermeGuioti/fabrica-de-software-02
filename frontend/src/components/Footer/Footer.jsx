f7ed7719331843c3ec7f25dffe740e1332c02174
GuilhermeGuioti <gmoroguioti@gmail.com>
2025-10-21 14:47:46 -0300

    subscribeForm footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Footer.css';
+
+import { supabase } from '../../supabaseClient';
 
 import { Link } from 'react-router-dom';
 import { FaInstagram, FaWhatsapp } from 'react-icons/fa';
 import { MdOutlineEmail } from 'react-icons/md';
-import { BsFillTelephoneFill, BsSend } from 'react-icons/bs'; // Adicionei o ícone de envio
+import { BsFillTelephoneFill, BsSend } from 'react-icons/bs';
 
 function Footer() {
+  const [email, setEmail] = useState('');
+  const [status, setStatus] = useState('idle');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if(!email) {
+      alert("Por favor insira um email");
+      return;
+    }
+
+    setStatus('loading');
+
+    const { data, error } = await supabase
+      .from('inscricoes')
+      .insert([
+        { email: email },
+      ]);  
+    
+    if(error){
+      console.error('Erro do Supabase:', error.message);
+      if(error.code == '23505') {
+        setStatus('duplicate');
+      } else {
+        setEmail('error')
+      }
+    } else{
+      console.log('Inscrito com sucesso:', data);
+      setStatus('success');
+      setEmail('');
+    } 
+  }
+
   return (
     <footer>
       <div className="footer-content">
@@ -95,12 +130,22 @@ function Footer() {
         <div className="footer-column">
           <h3>Inscreva-se</h3>
           <p>Receba nossas novidades e promoções por e-mail.</p>
-          <form className="subscribe-form">
-            <input type="email" placeholder="Seu melhor e-mail" />
-            <button type="submit" aria-label="Inscrever-se">
+          <form className="subscribe-form" onSubmit={handleSubmit}>
+            <input 
+              type="email" 
+              placeholder="Seu melhor e-mail" 
+              name='email'
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button type="submit" aria-label="Inscrever-se" disabled={status == 'loading'}>
               <BsSend />
             </button>
           </form>
+          {status === 'success' && <p className='feedback-message feedback-success'>Inscrito com sucesso!</p>}
+          {status === 'duplicate' && <p className='feedback-message feedback-warning'>Este e-mail já está inscrito.</p>}
+          {status === 'error' && <p className='feedback-message feedback-error'>Ocorreu um erro. Tente novamente.</p>}
         </div>
       </div>
 
