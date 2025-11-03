28a5a7fe11942f9fa5ff9c0148fe521613c83263
Guilherme Guioti <guimoroguioti@hotmail.com>
2025-11-03 19:14:00 -0300

    oi

--- frontend/src/pages/CatalogPage/CatalogPage.jsx
+++ frontend/src/pages/CatalogPage/CatalogPage.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductCard from '../../components/ProductCard/ProductCard.jsx';
 import { useProdutos } from '../../hooks/useProdutos.jsx';
 import './CatalogPage.css';
 
 import { FaSearch } from 'react-icons/fa';
+import { IoFilter } from "react-icons/io5";
 import HeroSecond from '../../components/HeroSecond/HeroSecond.jsx';
 
 const CatalogPage = () => {
   const { products, loading } = useProdutos();
+  const [searchTerm, setSearchTerm] = useState('');
 
   if (loading) {
     return (
@@ -20,6 +22,14 @@ const CatalogPage = () => {
     );
   }
 
+  const filteredProducts = products.filter((product) => {
+    const searchTermLower = searchTerm.toLocaleLowerCase();
+
+    const nameMatches = product.name.toLocaleLowerCase().includes(searchTermLower);
+
+    return nameMatches;
+  })
+
   return (
     <div className="catalog-page">
       <HeroSecond title={'Catálogo'} />
@@ -30,17 +40,27 @@ const CatalogPage = () => {
             <h2>Todos os Produtos</h2>
             <p>Encontre o móvel perfeito para você</p>
           </div>
-          <div className="catalog-header__search">
-            <input type="text" placeholder="Buscar no catálogo..." />
-            <button className='btn-search'>
-              <FaSearch />
-            </button>
+          <div className="catalog-header-container__search">
+            <div className="catalog-header__search">
+              <input 
+                type="text" 
+                placeholder="Buscar no catálogo..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)} 
+              />
+              <button className='btn-search'>
+                <FaSearch />
+              </button>
+            </div>
+              <button className='btn-filter'>
+                <IoFilter />
+              </button>
           </div>
         </header>
 
         <main className="catalog-main-content">
           <section className="catalog-page__grid">
-            {products.map((product) => (
+            {filteredProducts.map((product) => (
               <ProductCard
                 key={product.id}
                 imagemUrl={product.imageUrl}
